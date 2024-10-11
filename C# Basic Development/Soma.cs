a7faa966c382252e753db0a980511e0b6e7eb5c9
Renan Victor <80129254+Renanvictordiascosta@users.noreply.github.com>
2024-10-11 17:32:58 -0300

    Add files via upload

---
+++ C# Basic Development/Soma.cs
@@ -0,0 +1,32 @@
+﻿// Chamada das namespaces e classes
+using System;
+using System.Diagnostics.Contracts;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+// Criação do espaço para redigir as classes
+namespace ProjetoDeTeste {
+
+// Criação da classe
+    public class Soma {
+
+// Criação da função principal
+        public static void Main(string[] args) {
+
+// Solicitação para digitação de valor 1 
+            Console.Write("Digite o primeiro valor: ");
+            float x = float.Parse(Console.ReadLine());
+
+// Solicitação para digitação de valor 2
+            Console.Write("Digite o segundo valor: ");
+            float y = float.Parse(Console.ReadLine());
+
+// Geração de um terceiro valor com base nas atribuições dos dois anteriores
+            float z = (x + y) / 2;
+            Console.WriteLine("O valor da soma é " + z);
+
+        }
+
+    }
+
+}
